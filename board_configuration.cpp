0a996708090c2f52f0681f0b63bc6ecc391342db
kevinpolenghiengINEERING <kevin.polenghi.eng@gmail.com>
2025-12-22 14:28:55 +0100

    Update board_configuration.cpp

--- board_configuration.cpp
+++ board_configuration.cpp
@@ -2,46 +2,46 @@
 #include "board_overrides.h"
 
 Gpio getCommsLedPin() {
-	return Gpio::Unassigned;
+	return Gpio::E3;
 }
 
 Gpio getRunningLedPin() {
-	return Gpio::Unassigned;
+	return Gpio::E0;
 }
 
 Gpio getWarningLedPin() {
-	return Gpio::Unassigned;
+	return Gpio::E2;
 }
 
 // board-specific configuration setup
 static void customBoardDefaultConfiguration() {
-    // engineConfiguration->injectionPins[0] = Gpio::F13;
-    // engineConfiguration->ignitionPins[0] = Gpio::E15;
+     engineConfiguration->injectionPins[0] = Gpio::D12;
+    //engineConfiguration->ignitionPins[0] = Gpio::E15;
 
-//   engineConfiguration->triggerInputPins[0] = Gpio::B1;
+   engineConfiguration->triggerInputPins[0] = Gpio::A0;
 //	engineConfiguration->triggerInputPins[1] = Gpio::Unassigned;
 
-//	engineConfiguration->map.sensor.hwChannel = EFI_ADC_3;
+	engineConfiguration->map.sensor.hwChannel = EFI_ADC_2;
 
-//	engineConfiguration->clt.adcChannel = EFI_ADC_1;
+	engineConfiguration->clt.adcChannel = EFI_ADC_5;
 
-//	engineConfiguration->iat.adcChannel = EFI_ADC_2;
+	engineConfiguration->iat.adcChannel = EFI_ADC_7;
 
 
-    	// 5.6k high side/10k low side = 1.56 ratio divider
-  //  	engineConfiguration->analogInputDividerCoefficient = 1.56f;
+    	 5.6k high side/10k low side = 1.56 ratio divider
+    	engineConfiguration->analogInputDividerCoefficient = 1.56f;
 
     	// 6.34k high side/ 1k low side
 //    	engineConfiguration->vbattDividerCoeff = (6.34 + 1) / 1;
 
-//	engineConfiguration->adcVcc = 3.3f;
+	engineConfiguration->adcVcc = 3.3f;
 
-//	engineConfiguration->clt.config.bias_resistor = 2490;
-//	engineConfiguration->iat.config.bias_resistor = 2490;
+	//engineConfiguration->clt.config.bias_resistor = 2490;
+	//engineConfiguration->iat.config.bias_resistor = 2490;
 
 
-	// Battery sense on PA0
-//	engineConfiguration->vbattAdcChannel = EFI_ADC_0;
+	 Battery sense on PA4
+	engineConfiguration->vbattAdcChannel = EFI_ADC_4;
 }
 
 void setup_custom_board_overrides() {
